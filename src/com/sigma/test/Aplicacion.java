696a4ba4f007825caed2961aaa24469a1ae3563b
desarrollo1 <desarrollo1@desarrolloI>
2014-06-16 16:10:46 -0500

    Estoy probando

---
+++ src/com/sigma/test/Aplicacion.java
@@ -0,0 +1,11 @@
+package com.sigma.test;
+
+public class Aplicacion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hola Mundo");
+
+	}
+
+}
